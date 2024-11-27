4b7536b25b63cfde8beecd1ddb4246be0ef6fd0b
Роман Разогреев <razogreevroman@gmail.com>
2024-11-27 22:02:55 +0300

    bug fix

--- air defence 1.0/Missile.h
+++ air defence 1.0/Missile.h
@@ -65,6 +65,9 @@ public:
 	}
 	double missileMovingEquality(double xShift, double yShift, double x, uint8_t angleDeg, 
 								 double marchAccelerationApplyPointX, unsigned short timeS, double marchAccelerationX, double marchAccelerationY) const {
+		char missileState = 'u'; // u - up, d - down
+		double speed = 0;
+		double accelerationAngleRad = atan(marchAccelerationY / marchAccelerationX);
 		double angleRad = angleDeg * (PI / 180);
 		double yBallistic = 0.0;
 		double startAcceleration = this->missileSpeedMPS / this->missileActiveMotionTime;
@@ -76,36 +79,37 @@ public:
 		double accelerationLength = 0.0;
 		if (tan(angleRad) - ((((g_middle + (this->missileAirResistancePower / this->missileWholeMass))
 			/ (2 * this->missileSpeedMPS * this->missileSpeedMPS) * cos(angleRad) * cos(angleRad))) * (x - xShift)) > 0) {
-
+			missileState = 'u';
 		}
 		else if (tan(angleRad) - ((((g_middle + (this->missileAirResistancePower / this->missileWholeMass))
 					/ (2 * this->missileSpeedMPS * this->missileSpeedMPS) * cos(angleRad) * cos(angleRad) ) ) * (x - xShift)) < 0) {
-
+			missileState = 'd';
 		}
 		if (x - xShift < startLengthX) {
 			yBallistic = (x - xShift) * tan(angleRad) + yShift;
 			return yBallistic;
 		}
-		else if (x - xShift >= startLengthX) {
-			x -= startLengthX;
-			double arg1 = tan(angleRad) * (x - xShift);
-			double arg2 = g_middle + (this->missileAirResistancePower / this->missileWholeMass);
-			double arg3 = 2 * this->missileSpeedMPS * this->missileSpeedMPS * cos(angleRad) * cos(angleRad);
-			double arg4 = (x - xShift) * (x - xShift);
-			double arg5 = startLengthY + yShift;
+		x -= startLengthX;
+		double arg1 = tan(angleRad) * (x - xShift);
+		double arg2 = g_middle + (this->missileAirResistancePower / this->missileWholeMass);
+		double arg3 = 2 * this->missileSpeedMPS * this->missileSpeedMPS * cos(angleRad) * cos(angleRad);
+		double arg4 = (x - xShift) * (x - xShift);
+		double arg5 = startLengthY + yShift;
+		if (timeS >= 0 && marchAccelerationX != 0 && marchAccelerationY != 0) {
+			if (x >= marchAccelerationApplyPointX && x <= marchAccelerationApplyPointX + (speed * timeS) + 
+				(sqrt((marchAccelerationY * marchAccelerationY + marchAccelerationX * marchAccelerationX)) * timeS * timeS) / 2) {
+				yBallistic = arg1 - (((arg2 - marchAccelerationY) / arg3) * arg4) + arg5;
+				return yBallistic;
+			}
+		}
+		if (x - xShift >= startLengthX) {
 			yBallistic = arg1 - ((arg2 / arg3) * arg4) + arg5;
 			return yBallistic;
-		}
-		if (timeS >= 0 && marchAccelerationX != 0 && marchAccelerationY != 0) {
-			if (x >= marchAccelerationApplyPointX) {
-				yBallistic =
-			}
 		}
 		return yBallistic;
 	}
 	double calculateMissileTime(uint8_t angleDeg) const {
-		double result = (2 * this->missileSpeedMPS * sin(angleDeg * (PI / 180)) / g_middle) + 12;
-		return result;
+		return (2 * this->missileSpeedMPS * sin(angleDeg * (PI / 180)) / g_middle) + this->missileActiveMotionTime;
 	}
 };
 
