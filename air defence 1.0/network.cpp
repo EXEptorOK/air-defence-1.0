505e5bfca474fc68aa94bd8ce33e637728d5e2a2
Роман Разогреев <razogreevroman@gmail.com>
2024-12-15 16:51:48 +0300

    fucking curl

--- air defence 1.0/network.cpp
+++ air defence 1.0/network.cpp
@@ -1,4 +1,16 @@
+#pragma once
 #include "network.h"
+
+#pragma comment (lib,"Normaliz.lib")
+#pragma comment (lib,"Ws2_32.lib")
+#pragma comment (lib,"Wldap32.lib")
+#pragma comment (lib,"Crypt32.lib")
+
+#ifdef _DEBUG
+#    pragma comment (lib,"libcurl_a_debug.lib")
+#else
+#    pragma comment (lib,"libcurl_a.lib")
+#endif // _DEBUG
 
 static int writer(char* data, size_t size, size_t nmemb, std::string* writerData) {
     if (writerData == NULL) {
--- air defence 1.0/network.h
+++ air defence 1.0/network.h
@@ -3,19 +3,18 @@
 #include "Base.h"
 #include <locale>
 
-//#ifdef _DEBUG
-//#    pragma comment (lib,"lib/libcurl_a_debug.lib")
-//#else
-//#    pragma comment (lib,"curl/libcurl_a.lib")
-//#endif // _DEBUG
-
-/*Windows Specific Additional Depenedencies*/
-#pragma comment(lib, "libcurl.lib")
-#pragma comment (lib, "C://curl/curl-8.11.1/builds/libcurl-vc15-x64-release-static-ipv6-sspi-schannel/include/lib/libcurl_a.lib")
 #pragma comment (lib,"Normaliz.lib")
 #pragma comment (lib,"Ws2_32.lib")
 #pragma comment (lib,"Wldap32.lib")
 #pragma comment (lib,"Crypt32.lib")
+
+#ifdef _DEBUG
+#    pragma comment (lib,"lib/libcurl_a_debug.lib")
+#else
+#    pragma comment (lib,"lib/libcurl_a.lib")
+#endif // _DEBUG
+
+/*Windows Specific Additional Depenedencies*/
 
 static int writer(char* data, size_t size, size_t nmemb, std::string* writerData);
 int clientRequest(int argc, wchar_t* argv[]);;
